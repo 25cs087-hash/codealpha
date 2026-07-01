e8ef168a4c674c0e2505e9df52e15e6b85b83660
JANANI P <25cs087@rmkec.ac.in>
2026-07-01 11:25:53 +0530

    Add Customer class with properties and methods

---
+++ CodeAlpha_HotelReservationSystem/Customer.java
@@ -0,0 +1,35 @@
+public class Customer {
+
+    private String name;
+    private String phoneNumber;
+
+    // Constructor
+    public Customer(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer Name : " + name +
+               "\nPhone Number  : " + phoneNumber;
+    }
+}
